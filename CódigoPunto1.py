2deee90e558b065645467d67f4a2e540aaef3ca7
alejandradiazn <alejandra2240063@correo.uis.edu.co>
2025-04-18 22:37:10 -0500

    CAMBIOS

---
+++ CódigoPunto1.py
@@ -0,0 +1,103 @@
+#Árbol realizado con listas junto a sus correspondientes características.
+class Nodo:
+    def __init__(self, valor):
+        self.valor = valor
+        self.hijos = []
+
+class Arbol:
+    def __init__(self, valorRaiz):
+        self.raiz = Nodo(valorRaiz) #Esto permite que el valor del nodo se None ¿No?
+
+    
+
+    def agregarNodo(self, valor, padre):
+        if padre is None:
+            if self.raiz is None:
+                nuevo_nodo = Nodo(valor)
+                self.raiz = nuevo_nodo
+            else:
+                print("Debes tener una raíz para el árbol.")
+        else:
+            padre = self.buscarNodo(self.raiz, padre)
+            padre.hijos.append(Arbol(valor))
+            
+    def buscarNodo(self, nodo_actual, valor_buscado):
+        if nodo_actual is None:
+            return None
+        if nodo_actual.valor == valor_buscado:
+            return nodo_actual
+        for hijo_arbol in nodo_actual.hijos:  # hijo_arbol es un objeto Arbol ??? NEVERMIND FUNCIONA
+            resultado = self.buscarNodo(hijo_arbol.raiz, valor_buscado)  #TOCA PONER .RAIZ PARA RECONOICWER A HIJO COMO NODO
+            if resultado:
+                return resultado
+        return None
+                    
+
+
+    def imprimirArbol(self, nodo=None):
+        if nodo is None:
+            nodo = self.raiz
+        print(nodo.valor)
+        for hijo_arbol in nodo.hijos:
+            self.imprimirArbol(hijo_arbol.raiz)
+
+    def altura(self, nodo=None):
+        if nodo is None:
+            nodo = self.raiz
+        
+        if len(nodo.hijos) == 0:
+            return 1
+        else:
+            altura_hijos = []
+            for hijo in nodo.hijos:
+                altura_hijos.append(self.altura(hijo.raiz))
+            return 1 + max(altura_hijos)
+        
+
+    def peso(self, nodo = None):
+        if nodo is None:
+            nodo = self.raiz
+        totalNodos = 1
+        for hijo in nodo.hijos:
+            totalNodos += self.peso(hijo.raiz)
+        return totalNodos
+
+###Fingiendo que el orden corresponde al grado
+    def orden(self, nodo= None):
+        if nodo is None:
+            nodo = self.raiz
+        maxHijos = len(nodo.hijos)
+        for hijo in nodo.hijos:
+            maxHijos = max(maxHijos, self.orden(hijo.raiz))   
+        return maxHijos
+
+#Fragmento expuesto en consola.
+
+
+print("CREACIÓN DE ÁRBOL")
+valorRaiz = input("Ingrese el valor del nodo raiz.")
+arbol1 = Arbol(valorRaiz)
+
+while True:
+    inpt = input("Ingrese el valor de un nodo y el valor de su padre separados por un espacio o 'fin' para dejar de agreagar nodos.")
+    if inpt == "fin":
+        break
+    else:
+        if "," in inpt:
+            partes = inpt.split(",")
+            if len(partes) ==2:
+                hijo = partes[0]
+                padre = partes[1]
+            arbol1.agregarNodo(hijo, padre)
+
+print("\nÁRBOL EN PREORDEN.")
+arbol1.imprimirArbol()
+
+print("\nCARACTERÍSTICAS DEL ÁRBOL.")
+print(f"Peso del arbol: {arbol1.peso()}")
+print(f"Altura del arbol: {arbol1.altura()}")
+print(f"Orden del arbol: {arbol1.orden()}")
+
+
+
+
